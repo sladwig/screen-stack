bc997ba131c6704741ac246b480cc232473332c9
Stefan Ladwig <koenig.s@gmail.com>
2019-03-04 23:59:15 +0200

    feature: flexible height and updated readme

--- src/ScreenStack.js
+++ src/ScreenStack.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 
 const screenWidth = 300;
-const time = 500;
+const time = 285;
 function Abc() {
     const style = {
         background: 'red',
-        height: 500,
         width: screenWidth,
         flexShrink: 0,
     };
@@ -14,7 +13,6 @@ function Abc() {
 function Def() {
     const style = {
         background: 'green',
-        height: 500,
         width: screenWidth,
         flexShrink: 0,
     };
@@ -24,7 +22,6 @@ function Def() {
 function Xyz() {
     const style = {
         background: 'yellow',
-        height: 500,
         width: screenWidth,
         flexShrink: 0,
     };
@@ -38,29 +35,38 @@ function getScreen(type) {
         xyz: Xyz,
     }[type];
 }
+function getHeight(type) {
+    return {
+        abc: 500,
+        def: 400,
+        xyz: 300,
+    }[type];
+}
 
 export function ScreenStack() {
     const [stack, setStack] = useState(['abc', 'def', 'xyz']);
     const [viewIndex, setIndex] = useState(stack.length - 1);
 
+    const stackHeigth = getHeight(stack[viewIndex]);
     const stackStyle = {
         position: 'absolute',
         top: 20,
         left: 30,
-        height: 520,
+        alignItems: 'stretch',
         width: screenWidth,
+        height: '100%',
         overflow: 'hidden',
-        background: 'blue',
+        background: 'transparent',
+        // transition: `height ${time}ms ease`,
     };
     const style = {
         position: 'absolute',
         display: 'flex',
         left: -viewIndex * screenWidth,
         flex: 1,
-        background: 'blue',
-        transition: `left ${time}ms ease`,
+        height: stackHeigth,
+        transition: `left ${time}ms ease, height ${time}ms ease`,
     };
-    // setStack(['abc', 'def']);
 
     const push = newScreen => {
         const newStack = stack.slice();
@@ -73,8 +79,13 @@ export function ScreenStack() {
         const newStack = stack.slice();
         if (stack.length < 2) return;
         newStack.pop();
-        setIndex(Math.max(viewIndex - 1, 0));
-        setTimeout(() => setStack(newStack), time);
+        setIndex(viewIndex - 1);
+        setTimeout(() => {
+            // this could be made a bit better, so index and pop are more in tune
+            // if pressed multiple times
+            setStack(newStack);
+            setIndex(newStack.length - 1);
+        }, time);
     };
     const setReturnScreen = returnScreen => {
         const newStack = stack.slice();
@@ -95,7 +106,7 @@ export function ScreenStack() {
             </div>
             <div>
                 <div>
-                    stack {stack} {viewIndex}
+                    stack {stack} {viewIndex} {getHeight(stack[viewIndex])}
                 </div>
                 <div onClick={() => push('abc')}>push 'Abc'</div>
                 <div onClick={() => push('def')}>push 'Def'</div>
@@ -108,5 +119,4 @@ export function ScreenStack() {
             </div>
         </>
     );
-    // return <>'ABX'</>;
 }
